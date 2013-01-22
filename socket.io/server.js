8a4be42387fd850079b822e7fc70cd0a9fc36f4e
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2013-01-22 11:02:19 +0100

    Make the page ticks.

--- socket.io/server.js
+++ socket.io/server.js
@@ -18,8 +18,7 @@ function handler (req, res) {
 }
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+  setInterval(function(){
+    socket.emit('tick', 'miao');
+  }, 1000);
 });
